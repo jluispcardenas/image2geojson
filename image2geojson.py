9cab9f85ac31d598abaf64d218a5e0d1c1bc3b0c
Ubuntu <ubuntu@ip-172-26-4-161.ec2.internal>
2020-11-14 06:43:25 +0000

    Cambios

--- image2geojson.py
+++ image2geojson.py
@@ -8,10 +8,6 @@
 
 """
 A Python library to convert images to GeoJSON.
-a simple example::
-    import image2geojson
-
-    geojson = image2geojson.convert('image.png', lat=22.229, lng=-88.22, 17)
 
 """
 
@@ -35,20 +31,18 @@ def convert(file, output, lng, lat):
         sys.exit(1)
 
     im = cv2.imread(file)
-
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     #gray = cv2.medianBlur(gray, 5)
-
-    ret, th1 = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+    ret, th1 = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
 
     contours, hierarchy = cv2.findContours(th1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    geojson = {"type": "FeatureCollection", "features": []}
+    geojson = {"type": "FeatureCollection", "creator" : "image2geojson", "features": []}
     for cont in contours:
         if len(cont) < 3:
             continue
 
-        item = {"type": "Feature", "creator" : "image2geojson", "properties": None, "geometry": {"type": "Polygon", "coordinates": [[]]}}
+        item = {"type": "Feature", "properties": None, "geometry": {"type": "Polygon", "coordinates": [[]]}}
         coordinates = []
         for p in cont:
             coordinates.append(convert_to_lonlat(p[0][0], p[0][1], lng, lat))
@@ -66,11 +60,11 @@ def convert_to_lonlat(x, y, lng, lat):
     pi = math.pi
     r_earth = 6371000
 
-    lat_diff = (y / r_earth) * (180 / pi)
-    lng_diff = (x / r_earth) * (180 / pi) / math.cos(lat * pi/180)
+    lng_diff = (x / r_earth) * (180 / pi)
+    lat_diff = (y / r_earth) * (180 / pi) / math.cos(lat * pi/180)
 
-    new_lat = lat - lat_diff
     new_lng = lng + lng_diff
+    new_lat = lat - lat_diff
 
     return (new_lng, new_lat)
 
